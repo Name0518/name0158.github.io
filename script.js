a582241d63ca55d019358effc720961a32de23da
Name69 <tangmm7799@gmail.com>
2025-08-12 23:54:49 -0400

    Code section 70% done

--- script.js
+++ script.js
@@ -8,46 +8,71 @@ const sensBack = 1000
 
 const mediaQuery = window.matchMedia("(min-width: 768px)");
 
-if (mediaQuery.matches){
-mouse.addEventListener('mousemove', e=> {
-    const x = e.clientX;
-    const y = e.clientY;
+if (mediaQuery.matches) {
+    mouse.addEventListener('mousemove', e => {
+        const x = e.clientX;
+        const y = e.clientY;
 
-    front.style.transform = `
+        front.style.transform = `
     translate(
         ${-x/sensFront}%,
          ${-y/sensFront}%
-    )` ; 
-    back.style.transform = `
+    )`;
+        back.style.transform = `
     translate(
         ${
            x/sensBack
         }%, ${y/sensBack
-}% ) ` ;
-})
+}% ) `;
+    })
 }
 
 
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
-function changeBackground(scrollPos) {
-  if (scrollPos > 500 && scrollPos < 2000) {
-    document.body.style.backgroundImage = "url('images/programmador.jpg')";
-  } else {
-    document.body.style.backgroundImage = ""; // reset to default
-  }
+
+function changeTheme(scrollPos) {
+    const cards = document.querySelectorAll(".card"); // get all cards
+    const projectCard = document.querySelectorAll(".projectCard");
+    if (scrollPos > 500 && scrollPos < 2000) {
+        document.body.style.backgroundImage = "url('images/programmador.jpg')";
+        cards.forEach(card => {
+            card.style.backgroundColor = "rgba(61, 61, 61, 0.75)";
+            card.style.borderColor = ""; // white border
+            card.style.color = "";
+        });
+
+        projectCard.forEach(pc => {
+            pc.style.backgroundColor = "";
+            pc.style.borderColor = "#ffffff";
+            pc.style.color = "#000000ff";
+        });
+    } else {
+        document.body.style.backgroundImage = ""; // reset to default
+        cards.forEach(card => {
+            card.style.backgroundColor = "rgba(224, 224, 224,0.9)";
+            card.style.borderColor = "#ffffff"; // white border
+            card.style.color = "#000000ff";
+        });
+
+        projectCard.forEach(pc => {
+            pc.style.backgroundColor = "#ffffff";
+            pc.style.borderColor = "#000000";
+            pc.style.color = "#000000";
+        });
+    }
 }
 
 document.addEventListener("scroll", () => {
-  lastKnownScrollPosition = window.scrollY;
+    lastKnownScrollPosition = window.scrollY;
 
-  if (!ticking) {
-    window.requestAnimationFrame(() => {
-      changeBackground(lastKnownScrollPosition);
-      ticking = false;
-    });
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            changeTheme(lastKnownScrollPosition);
+            ticking = false;
+        });
 
-    ticking = true;
-  }
+        ticking = true;
+    }
 });
